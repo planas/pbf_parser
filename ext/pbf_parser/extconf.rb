e9a9d5fdc4ccc682178c56290a790ada5f28983a
Sukrit Sunama <sunama.sukrit@gmail.com>
2022-12-26 10:55:00 +0700

    Update extconf.rb

--- ext/pbf_parser/extconf.rb
+++ ext/pbf_parser/extconf.rb
@@ -15,7 +15,7 @@ LIB_DIRS = [
   '/opt/local/lib',
   '/usr/local/lib',
   '/usr/lib',
-  '/opt/homebrew/include',
+  '/opt/homebrew/lib',
   LIBDIR
 ]
 
@@ -25,7 +25,7 @@ dir_config('zlib', HEADER_DIRS, LIB_DIRS)
 abort "protobuf-c is required" unless find_header('protobuf-c/protobuf-c.h')
 abort "zlib is required"       unless find_header('zlib.h')
 
-#abort "protobuf-c is required" unless find_library('protobuf-c', 'protobuf_c_message_unpack')
+abort "protobuf-c is required" unless find_library('protobuf-c', 'protobuf_c_message_unpack')
 abort "zlib is required"       unless find_library('z', 'inflate')
 
 create_makefile('pbf_parser/pbf_parser')
